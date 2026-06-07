0276da9d997b4c378d3fbe709542eb7e049a574c
Zulqarnain04 <zulqarnainCyrus@gmail.com>
2026-06-08 00:14:36 +0800

    all

---
+++ backend/src/test/java/com/flood_resilience/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flood_resilience.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
